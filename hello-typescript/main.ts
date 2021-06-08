d192ec15f02b6c5c6a31fd4f7a2c603d62e7267a
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-08 14:45:03 -0300

    TypeScript Tests - 4º Class

---
+++ hello-typescript/main.ts
@@ -0,0 +1,26 @@
+//Teste de funcionalidade do TypeScript. Fonte: http://loiane.training
+
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+//ES6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor){
+    return valor*2;
+})
+
+numeros.map(valor => valor*2); //ES 2015
+
+class Matematica{
+    soma(x, y){
+        return x + y;
+    }
+}
+
+var n1: string = 'String';
