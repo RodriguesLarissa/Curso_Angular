e6f7910d28cfcc161a559dcf26f8d2ebfe378b0c
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-08 15:26:39 -0300

    Modules - 5° Class

--- primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -5,7 +5,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'meu-primeiro-component',
     template: `
-        <p>Esse é meu primeiro component com Angular 2! &nbsp;</p>
+        <p>Esse é meu primeiro component com Angular 2!</p>
     `
 })
 export class MeuPrimeiroComponent { }
