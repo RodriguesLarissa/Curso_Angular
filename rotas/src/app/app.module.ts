bcb2f438f42da26f3dfaa4efa9fb2435ed46de8e
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-15 13:49:03 -0300

    50º Class - Routes

---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { routing } from './app.routing';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MaterializeModule } from 'angular2-materialize';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterializeModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
