7f1f2b27cb2cb67d47d18163be84dedcaa65c069
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-15 11:08:10 -0300

    47º Class - Impure Pipes

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -9,14 +9,16 @@ registerLocaleData(localePt, 'pt-BR');
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
-import { FiltroArrayPipe } from './filtro-array.pipe'
+import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemplosPipesComponent,
     CamelCasePipe,
-    FiltroArrayPipe
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
     BrowserModule,
--- pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -10,7 +10,7 @@ export class ExemplosPipesComponent implements OnInit {
   filme: any = {
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
     rating: 4.54321,
-    tempo: 2.33,
+    tempo: 2,
     dataLancamento: new Date(2004, 6, 4),
     sinopse: 'O 3º ano de ensino na Escola de Magia e Bruxaria de Hogwarts se aproxima. Porém um grande perigo ronda a escola: o assassino Sirius Black (Gary Oldman) fugiu da prisão de Azkaban, considerada até então como à prova de fugas. Para proteger a escola são enviados os Dementadores, estranhos seres que sugam a energia vital de quem se aproxima deles, que tanto podem defender a escola como piorar ainda mais a situação.',
     precoLivro: 30.00
@@ -22,6 +22,21 @@ export class ExemplosPipesComponent implements OnInit {
 
   addFilme(valor: string){
     this.filmes.push(valor);
+  }
+
+  obterFilmes(){
+
+    if(this.filmes.length === 0 || this.filtro === undefined || this.filtro.trim() === ''){
+      return this.filmes;
+    }
+
+    return this.filmes.filter((v) => {
+      if(v.toLowerCase().indexOf(this.filtro.toLowerCase()) >= 0){
+        return true;
+      }
+      return false;
+    });
+
   }
 
   constructor() { }
