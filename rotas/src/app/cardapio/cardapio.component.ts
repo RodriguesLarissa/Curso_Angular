b59f003aacdc56f8f16265ccd42a6d87919b41f5
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-16 09:16:06 -0300

    56º Class - URL Parameters

--- rotas/src/app/cardapio/cardapio.component.ts
+++ rotas/src/app/cardapio/cardapio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { CardapioService } from './cardapio.service';
 
@@ -10,11 +12,32 @@ import { CardapioService } from './cardapio.service';
 export class CardapioComponent implements OnInit {
 
   cardapio: any[] = [];
+  pagina: number = 0;
+  inscricao: Subscription = new Subscription;
 
-  constructor(private cardapioService: CardapioService) { }
+  constructor(
+    private cardapioService: CardapioService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.cardapio = this.cardapioService.getCardapio();
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) => {
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
+  }
+
+  proximaPagina(){
+    //this.pagina++;
+    this.router.navigate(['/cardapios'],
+      {queryParams: {'pagina': ++this.pagina}});
   }
 
 }
