2b26efd8ea49e84612b48b094316a9a098773495
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-17 10:31:02 -0300

    64º Class - Login Screen and Hidden NavBars

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { AppRoutingModule } from './app.routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterializeModule } from 'angular2-materialize';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './login/auth.service';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     MaterializeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  //providers: [CardapioService],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 
