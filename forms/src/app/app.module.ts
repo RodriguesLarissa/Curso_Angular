b2068eee765032f440069c702bac9c5e0828c550
Larissa Rodrigues de Almeida <larissa.rodrigues@concert.com.br>
2021-06-18 15:07:07 -0300

    81º Class - Verify form data with JSON

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
 import { TemplateFormModule } from './template-form/template-form.module';
 
-
 @NgModule({
   declarations: [
     AppComponent,
